0f0a1ff873fbb71a757d47788933b99368890493
Miyuki <yuzh7799@gmail.com>
2021-12-06 23:58:43 +0800

    first commit

---
+++ src/main/java/com/miyuki/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.miyuki.git;
+
+import org.w3c.dom.ls.LSOutput;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: miyuki
+ * @Date: 2021/12/06/23:47
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
